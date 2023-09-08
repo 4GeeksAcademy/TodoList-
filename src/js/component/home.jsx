03687fec3dc695dc652e28dd6383a1eac4aca30d
FrancoRegg <138858381+FrancoRegg@users.noreply.github.com>
2023-09-08 09:18:06 +0000

    Elimino los componenetes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,22 +1,11 @@
 import React from "react";
-import Title from "./Title";
-import TodoInput from "./TodoInput";
-import TodoList from "./TodoList";
-import Todo from "./Todo";
+
 //create your first component
 
 const Home = () => {
   
   return(
-  <div className="container">
-    <div>
-      <Title />
-      <TodoInput />
-      <TodoList>
-        <Todo />
-      </TodoList>
-    </div>
-  </div>
+ 
   )
     
 };
