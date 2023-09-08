ea00c53d63c7e0db0c1b2aa12a1aa58dd65b5a28
FrancoRegg <138858381+FrancoRegg@users.noreply.github.com>
2023-09-08 10:02:21 +0000

    estructura jsx creada

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import list from "./list";
+import { element } from "prop-types";
 
 //create your first component
 const Home = () => {
@@ -19,9 +20,21 @@ const Home = () => {
   };
   
   return(
- 
-  )
-    
-};
+    <div className="container">
+      <h1>list tasks</h1>
+      <input type="text" placeholder="Add new tasks..." onKeyDown={enterInput} onChange={(e)=>setInputValue(e.target.value)} value={inputValue}/>
+
+      <ul className="todoList">
+        {tasks.map((element, index) =>{
+          return <list text={element} deleteItem={deleteItem} itemIndex={index} />
+        })};
+      </ul>
+
+      <div className="footer">
+        {tasks.length} items left.
+      </div>
+    </div>
+
+  )};
 
 export default Home;
