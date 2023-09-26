0bce14e72caf98121cf133d3e937bd2618c96a0b
FrancoRegg <138858381+FrancoRegg@users.noreply.github.com>
2023-09-26 17:48:00 +0000

    La app vuelve a funcionar correctamente

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,37 +7,42 @@ const Home = () => {
 
   const [inputValue, setInputValue] = useState("");
   const [tasks, setTasks] = useState([])
-  const [task, setTask] = useState([])
+
 
   const enterInput = (event) => {
     if (event.key === "Enter" && inputValue) {
       setTasks([...tasks, inputValue])
-      setInputValue([])
+      setInputValue("")
     };
   };
 
   const deleteTask = (event) => {
-    parseInt(event.target.getAttribute("data-remove-id"));
-  };
+    let index = parseInt(event.target.getAttribute("data-remove-id"));
+    if (!isNaN(index)) {
+      const updatedTasks = [...tasks]; // Hacer una copia de las tareas
+      updatedTasks.splice(index, 1); // Eliminar la tarea en el índice
+      setTasks(updatedTasks); // Actualizar el estado con las tareas actualizadas
+    }
+  
+    };
 
-
-  return (
-    <div className="cont container-fluid vh-100 d-flex justify-content-center">
-      <div className="containerTodo bg-white m-auto p-5">
-        <h1>List tasks</h1>
-        <input type="text" placeholder="Add new tasks..." onKeyDown={enterInput} onChange={(event) => setInputValue(event.target.value)} value={inputValue} />
-        <ul className="todolist my-3">
-          {tasks.map((element, index) => {
-            return <List text={element} deleteTask={deleteTask} itemIndex={index} />
-          })}
-        </ul>
-        <div className="footer">
-          <div>{tasks.length} items left.</div>
-          <button className="">Delete all list</button>
+    return (
+      <div className="cont container-fluid vh-100 d-flex justify-content-center">
+        <div className="containerTodo bg-white m-auto p-5">
+          <h1>List tasks</h1>
+          <input type="text" placeholder="Add new tasks..." onKeyDown={enterInput} onChange={(event) => setInputValue(event.target.value)} value={inputValue} />
+          <ul className="todolist my-3">
+            {tasks.map((element, index) => {
+              return <List key={index} text={element} deleteTask={deleteTask} itemIndex={index} />
+            })}
+          </ul>
+          <div className="footer">
+            <div>{tasks.length} items left.</div>
+            <button className="">Delete all list</button>
+          </div>
         </div>
       </div>
-    </div>
-  )
-};
+    )
+  };
 
-export default Home;
+  export default Home;
