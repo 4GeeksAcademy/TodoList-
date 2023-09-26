e0330948aeb6f0e86c2b4b138d5916184c0fec6c
FrancoRegg <138858381+FrancoRegg@users.noreply.github.com>
2023-09-26 16:23:44 +0000

    Revisar proyecto, dejó de funcionar y no encuentro el error

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,56 +1,43 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import List from "./list"
 
 
 //create your first component
 const Home = () => {
 
-  const [inputValue, setInputValue] = useState ('');
-  const [task, setTask] = useState ([])
-  const [tasks, setTasks] = useState ([])
-  
-  /*Hago una llamada a la api para que me traiga la informacion del usuario que yo le asigne*/ 
-  useEffect (() => {
-    fetch("https://playground.4geeks.com/apis/fake/todos/user/fraanuni")
-    .then(response => response.json())
-    .then(data => setTasks(data))
-    .catch(err => err)
-  },[])
-
-  /*Hago una llamada a la api para que modifique mi lista cada ver que agrege una nueva tarea*/
-  
-
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks] = useState([])
+  const [task, setTask] = useState([])
 
   const enterInput = (event) => {
-    if(event.key === "Enter" && inputValue){
-      setTask([...task, inputValue])
-      setInputValue("")
+    if (event.key === "Enter" && inputValue) {
+      setTasks([...tasks, inputValue])
+      setInputValue([])
     };
   };
 
   const deleteTask = (event) => {
-		let position = parseInt(event.target.getAttribute("data-remove-id"));
-		setTask(task.filter((item, index) => index !== position))
-	};
+    parseInt(event.target.getAttribute("data-remove-id"));
+  };
 
 
-  return(
+  return (
     <div className="cont container-fluid vh-100 d-flex justify-content-center">
       <div className="containerTodo bg-white m-auto p-5">
         <h1>List tasks</h1>
         <input type="text" placeholder="Add new tasks..." onKeyDown={enterInput} onChange={(event) => setInputValue(event.target.value)} value={inputValue} />
-
         <ul className="todolist my-3">
-					{tasks.map((element, index) => {
-						return <List text={element.label} deleteTask={deleteTask} itemIndex={index}/>
-					})}
-				</ul>
+          {tasks.map((element, index) => {
+            return <List text={element} deleteTask={deleteTask} itemIndex={index} />
+          })}
+        </ul>
         <div className="footer">
-          {task.length} items left.
-          <button className="text-end">Delete all list</button>
-        </div>   
+          <div>{tasks.length} items left.</div>
+          <button className="">Delete all list</button>
+        </div>
       </div>
     </div>
-  )};
+  )
+};
 
 export default Home;
